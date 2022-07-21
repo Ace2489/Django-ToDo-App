cb7bfe70a184840678962298f3d2245e3b3da9d8
Covenant Okoroafor <okoroaforcovenant99@gmail.com>
2022-07-21 14:18:57 +0100

    Initial Project Setup and Task-Model Creation:
    -Initialised the entire project
    -Created the base app
    -Created model for Task

---
+++ base/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User 
+
+# Create your models here.
+class Task(models.Model):
+    User = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    Title = models.CharField(max_length=150)
+    Description = models.TextField()
+    Completed = models.BooleanField()
+    Created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.Title
