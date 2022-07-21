e3cdfea4c14172fe014f2a89d18c7e37cd063fd1
Covenant Okoroafor <okoroaforcovenant99@gmail.com>
2022-07-21 22:09:53 +0100

    Added CBV and FBV for Creation of Tasks

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.urls import path
 
-urlpatterns = [path('', views.getTasks, name = 'tasks'),
-               path('<int:pk>',views.getTaskDetails, name = 'details') 
+urlpatterns = [path('', views.TaskList.as_view(), name = 'tasks'),
+               path('<int:pk>',views.TaskDetails.as_view(), name = 'details'),
+               path('create',views.TaskCreate.as_view(), name = 'create') 
                 ]
--- base/views.py
+++ base/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-
+from django.shortcuts import redirect, render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+from .forms import TaskForm
 from .models import Task
+
 
 # Create your views here.
 class TaskList(ListView):
@@ -22,3 +23,20 @@ class TaskDetails(DetailView):
 def getTaskDetails(request, pk):
     task = Task.objects.get(id = pk)
     return render(request, 'task_detail.html', {'task':task})
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+    context_object_name = 'form'
+
+def createTask(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('tasks'))
+    else:
+        form = TaskForm()
+    context = {'form':form}
+    return render(request, 'task_form.html', context)
