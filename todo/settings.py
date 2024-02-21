e4332647613172ab9a76f12ffc4d61d85523f916
Covenant Okoroafor <Okoroaforcovenant99@gmail.com>
2024-02-21 17:25:44 +0100

    Changed the DB from PG to Sqlite3 for easy setup

--- todo/settings.py
+++ todo/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'By526iF9hgQ9kidnSwz3',
-        'HOST': 'containers-us-west-73.railway.app',
-        'PORT': '6057',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
 
